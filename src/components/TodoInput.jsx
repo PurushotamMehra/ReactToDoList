a3165787de0c04dfd0fdd4e4d40ff892049cebab
PurushotamMehra <purushotam.mehra2001@gmail.com>
2024-08-06 00:33:18 +0530

    Normalize line endings

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+export default function TodoInput(props){
+    const { handleAddTodos, todoValue, setTodoValue } = props
+    // const [todoValue, setTodoValue] = useState('')
+    return (
+        <header>
+            <input value={todoValue} onChange={(e) => {setTodoValue(e.target.value)}} placeholder = "Enter Todo..."/>
+            <button onClick={() => {handleAddTodos(todoValue)}}>Add</button>
+        </header>
+    )
+}
+
